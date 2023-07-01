c4b3467cde45ef84c16bbc02ad15aba3e632f74e
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-07-02 04:11:22 +0530

    done with header tab and cursor

---
+++ Incheq Contact/scripts/index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let mousePosX = 0,
+        mousePosY = 0,
+        mouseCircle = document.getElementById('mouse-circle');
+
+    document.onmousemove = (e) => {
+        mousePosX = e.pageX;
+        mousePosY = e.pageY;
+    }
+
+    mouseCircle.style.top = mousePosX + 'px';
+    mouseCircle.style.left = mousePosY + 'px';
+
+    let delay = 4,
+        revisedMousePosX = 0,
+        revisedMousePosY = 0;
+
+    function delayMouseFollow() {
+        requestAnimationFrame(delayMouseFollow);
+
+        revisedMousePosX += (mousePosX - revisedMousePosX-15) / delay;
+        revisedMousePosY += (mousePosY - revisedMousePosY-7) / delay; 
+
+        mouseCircle.style.top = revisedMousePosY + 'px';
+        mouseCircle.style.left = revisedMousePosX + 'px';
+    }
+    delayMouseFollow();
+});
